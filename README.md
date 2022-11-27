5d44b0e3b68223ceee729846b3972b4c01262d5e
Dhruv <dhruvkhanna0806@gmail.com>
2022-11-27 20:38:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# react-typescript-e-cart
---
+++ src/pages/Store.tsx
@@ -0,0 +1,17 @@
+import { Row, Col } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
+import storeItems from "../data/items.json";
+export function Store() {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row ms={2} sx={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
